5b2e46dfe0af0e4f77be57f92033601205804154
contactlumio37-droid <contact.lumio37@gmail.com>
2026-03-31 07:43:41 +0200

    refactor: remove hardcoded Firebase keys, import from firebase.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBHlFlN_oSP9p5RSUwJgnHPQkIdEWTJRIU",
-  authDomain: "lumio-8ed03.firebaseapp.com",
-  projectId: "lumio-8ed03",
-  storageBucket: "lumio-8ed03.firebasestorage.app",
-  messagingSenderId: "1076911403682",
-  appId: "1:1076911403682:web:823a3ff4db2f1e09859dfb",
-  measurementId: "G-BJZFS6H8C0"
-};
+// Firebase is initialized via src/firebase.js
+import "./firebase";
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
